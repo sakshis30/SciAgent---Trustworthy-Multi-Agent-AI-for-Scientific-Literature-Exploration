29368850de71b4c3ff42bbc7cf6d8bdc13116bc3
sakshi <singhsakshi.0130@gmail.com>
2026-07-16 22:55:08 +0200

    Implement PDF parser using PyMuPDFLoader

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+from parser import PDFParser
 
+def main():
+
+    parser = PDFParser("data/papers/attention_is_all_you_need.pdf")
+
+    documents = parser.load()
+
+    print(f"Loaded {len(documents)} pages.\n")
+    print(documents[0].page_content[:1000])
+
+if __name__ == "__main__":
+    main()
--- src/parser.py
+++ src/parser.py
@@ -1,1 +1,18 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyMuPDFLoader
+
+class PDFParser:
+    "Responsible for loading documents"
+
+    def __init__(self, pdf_path: str):
+        self.pdf_path = Path(pdf_path)
+    
+    def load(self):
+        "Loads the PDF and returns Langchain documents"
+
+        loader = PyMuPDFLoader(str(self.pdf_path))
+        documents = loader.load()
+
+        return documents
+        
 
